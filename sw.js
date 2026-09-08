04cc85be50f9baee3a30ed929f5b4a3b2ae2b75e
Mor Ndiaye <bigmor24762@gmail.com>
2026-09-08 06:55:56 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('khelcom-v1').then(cache => {
-    return cache.addAll(['khelcom.html', 'manifest.json', 'logo-khelcom.jpg']);
-  }));
+const CACHE_NAME = 'khelcom-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo-khelcom.jpg',
+  './logo192.png',
+  './logo512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
